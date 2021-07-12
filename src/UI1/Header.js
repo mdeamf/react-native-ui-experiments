c3cc02d03d09e69332c86c0225ac4ef3ba35e1a7
mdeamf <mauriciodealmeidamf@gmail.com>
2021-07-12 14:58:09 -0300

    feat: adding header

---
+++ src/UI1/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.menu} onPress={() => console.log('test')}>
+        <FontAwesomeIcon size={18} color="#0e43a1" icon={['fas', 'bars']} />
+      </TouchableOpacity>
+
+      <View style={styles.user}>
+        <TouchableOpacity onPress={() => console.log('test')}>
+          <FontAwesomeIcon
+            style={styles.userNotifications}
+            size={24}
+            color="#0e43a1"
+            icon={['far', 'bell']}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log('test')}>
+          <Image
+            style={styles.userAvatar}
+            source={{uri: 'https://i.pravatar.cc/300'}}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  menu: {
+    backgroundColor: '#eff1ff',
+    padding: 16,
+    borderRadius: 50,
+  },
+  user: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userNotifications: {
+    marginRight: 32,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+});
+
+export default Header;
--- src/UI1/HomePage.js
+++ src/UI1/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {ScrollView, Text} from 'react-native';
+import Header from './Header';
 
 const HomePage = () => {
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={{padding: 24}}>
+        <Header />
         <Text>Bem-vindo</Text>
         <Text>Maurício Meira</Text>
       </ScrollView>
