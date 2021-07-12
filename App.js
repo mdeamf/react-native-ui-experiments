702fc1f240436cdd477e6c682fbf658fc92e6941
mdeamf <mauriciodealmeidamf@gmail.com>
2021-07-12 14:57:06 -0300

    chore: adding font-awesome

--- App.js
+++ App.js
@@ -11,6 +11,12 @@ import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import HomePage from './src/UI1/HomePage';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fab, fas, far);
+
 const App = () => {
   return (
     <SafeAreaProvider>
